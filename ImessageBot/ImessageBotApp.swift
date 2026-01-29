7470e8e85a9100017408fe0332939de44bd073f2
LiPan <lipan@adsit.cn>
2026-01-30 00:13:25 +0800

    Initial Commit

---
+++ ImessageBot/ImessageBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImessageBotApp.swift
+//  ImessageBot
+//
+//  Created by Mac on 2026/1/30.
+//
+
+import SwiftUI
+
+@main
+struct ImessageBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
